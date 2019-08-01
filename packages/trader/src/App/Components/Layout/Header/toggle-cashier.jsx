56b16f6bdbdcc2b0dc1cdb1670b5a7c815953ff8
Mohammadreza Ghorbani <reza@firstsource.io>
2019-08-01 15:55:15 +0430

    Fix unmounting modal on every actio

--- packages/trader/src/App/Components/Layout/Header/toggle-cashier.jsx
+++ packages/trader/src/App/Components/Layout/Header/toggle-cashier.jsx
@@ -2,11 +2,10 @@ import classNames         from 'classnames';
 import PropTypes          from 'prop-types';
 import React              from 'react';
 import { localize }       from 'App/i18n';
+import Lazy               from 'App/Containers/Lazy';
 import { Modal }          from 'App/Components/Elements/modal.jsx';
-import {
-    Deposit,
-    Withdrawal }          from 'App/Containers/CashierModal';
 import Button             from '../../Form/button.jsx';
+import UILoader           from '../../Elements/ui-loader.jsx';
 
 const WalletInformation = React.lazy(() => import(/* webpackChunkName: "wallet-information" */'Modules/Reports/Containers/wallet-information.jsx'));
 const tabs = {
@@ -17,11 +16,25 @@ const modal_content = [
     {
         icon : 'IconDepositSmall',
         label: localize('Deposit'),
-        value: Deposit,
+        // eslint-disable-next-line react/display-name
+        value: () => (
+            <Lazy
+                ctor={() => import('App/Containers/CashierModal/deposit.jsx')}
+                should_load={true}
+                has_progress={true}
+            />
+        ),
     }, {
         icon : 'IconWithdrawalSmall',
         label: localize('Withdrawal'),
-        value: Withdrawal,
+        // eslint-disable-next-line react/display-name
+        value: () => (
+            <Lazy
+                ctor={() => import('App/Containers/CashierModal/withdrawal.jsx')}
+                should_load={true}
+                has_progress={true}
+            />
+        ),
     },
 ];
 const ToggleCashier = ({
@@ -39,17 +52,21 @@ const ToggleCashier = ({
             text={localize('Deposit')}
             onClick={() => { toggleCashier('deposit'); }}
         />
-        <Modal
-            className='cashier'
-            modal_content={modal_content}
-            header={<WalletInformation />}
-            hideFullBlur={hideFullBlur}
-            is_open={is_cashier_visible}
-            selected_index={tabs[active_tab]}
-            showFullBlur={showFullBlur}
-            title={localize('Cashier')}
-            toggleModal={toggleCashier}
-        />
+        <React.Suspense fallback={<UILoader />}>
+            <Modal
+                className='cashier'
+                modal_content={modal_content}
+                header={<WalletInformation />}
+                hideFullBlur={hideFullBlur}
+                is_open={is_cashier_visible}
+                selected_index={tabs[active_tab]}
+                showFullBlur={showFullBlur}
+                title={localize('Cashier')}
+                toggleModal={toggleCashier}
+                tag_name='cashier_modal'
+            />
+        </React.Suspense>
+
     </React.Fragment>
 );
 
