ee50053d51aa2a2bb81257ad7ce235671b75479a
negar <negar@binary.com>
2019-10-16 11:54:40 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("/workbox-v4.3.1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/workbox-v4.3.1"});
 
 importScripts(
-  "/precache-manifest.6ee3c6bcebad714dc33cc62575656357.js"
+  "/precache-manifest.42d03823e024a95da6d998ea53d7ecbf.js"
 );
 
 workbox.core.skipWaiting();
