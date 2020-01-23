6dd781e36386e78680761219b0a1975f1f50fe50
negar <negar@binary.com>
2020-01-23 14:27:52 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.068b1c64aa82b7032e568d53ad2f9a16.js"
+  "/precache-manifest.a6edc6e2fe2abbdb9a18dc5635882914.js"
 );
 
 workbox.core.skipWaiting();
